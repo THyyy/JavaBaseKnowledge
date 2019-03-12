68f1f45c204744ec304ec602bc05fbb0a1f2b81e
THyyy <tanheyuan@outlook.com>
2019-03-12 17:56:53 +0800

    '添加生产者消费者模式以及部分笔试题目'

---
+++ src/main/java/designmode/producerandconsumer/Producer.java
@@ -0,0 +1,41 @@
+package designmode.producerandconsumer;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author THY
+ * @date 2019/3/12
+ */
+public class Producer implements Runnable{
+    private boolean working = true;
+    private BlockingQueue blockingQueue;
+    private static AtomicInteger count = new AtomicInteger();
+    //构造函数
+
+    public Producer(BlockingQueue blockingQueue) {
+        this.blockingQueue = blockingQueue;
+    }
+
+    @Override
+    public void run() {
+        while(working) {
+            int id = count.incrementAndGet();
+            Food food = new Food(id);
+            if (blockingQueue.offer(food)){
+                System.out.println(Thread.currentThread().getId() + "号员工将" + food.getId() + "号食物放入餐桌");
+            }else {
+                System.out.println("餐桌已满，" + food.getId() + "无法放入餐桌");
+            }
+            try {
+                Thread.sleep(3 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void stop(){
+        working = false;
+    }
+}
---
+++ src/main/java/designmode/producerandconsumer/Service.java
@@ -0,0 +1,38 @@
+package designmode.producerandconsumer;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * @author THY
+ * @date 2019/3/12
+ */
+public class Service {
+    public static void main(String[] args) {
+        BlockingQueue<Food> queue = new LinkedBlockingDeque<>(10);
+
+        Producer[] producers = new Producer[4];
+        Consumer[] consumers = new Consumer[20];
+
+        for (int i = 0; i < 4; i++) {
+            producers[i] = new Producer(queue);
+        }
+        for (int j = 0; j < 20; j++) {
+            consumers[j] = new Consumer(queue);
+        }
+        ExecutorService executorService = Executors.newFixedThreadPool(6);
+        for (int i = 0; i < 4; i++) {
+            executorService.execute(producers[i]);
+        }
+        for (int j = 0; j < 20; j++) {
+            executorService.execute(consumers[j]);
+        }
+        try {
+            Thread.sleep(1000 * 20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
